0a9e4f9e0333fcfe70cae34d7f0959dc269f5a04
Reggie <rpind002@odu.edu>
2025-05-13 16:16:13 -0400

    Change to views for requests

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+import requests
 from django.conf import settings
 from rest_framework import status
 from django.db.models import F
